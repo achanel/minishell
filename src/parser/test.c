93a65ae49f540098fd3771ff5135e7a7e1ca572b
rhoke <rhoke@student.21-school.ru>
2021-12-29 20:05:07 +0300

    exexveintest

--- src/parser/test.c
+++ src/parser/test.c
@@ -5,6 +5,7 @@ int main(int argc, char const *argv[], char **envp)
 	char *str = ft_strdup("echo lol kek");
 	char **str1 = args_split(str, ' ');
 	// write(0, str, ft_strlen(str));
+	write(1,'lol', 3);
 	char *cmd;
 	
 	execve(get_path(envp, str1[0]), str1, envp);
