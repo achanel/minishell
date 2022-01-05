62e73c589c103f679f8ab6138454230c5da281a7
rhoke <rhoke@student.21-school.ru>
2022-01-05 17:54:09 +0300

    redirect_test.c

--- src/parser/redirects.c
+++ src/parser/redirects.c
@@ -1,53 +1,84 @@
 #include "../../includes/minishell.h"
 
-char *str2str(char *str)
+char *str2str(char *str, int *i1, char c)
 {
 	int i = 0, j = 0;
 	char *tmp;
 	
-
+	i = *i1;
 	tmp = malloc(ft_strlen(str));
+
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\'' || str[i] == '\"')
 		i++;
-	while(str[i] == '<')
+	while(str[i] == c)
 		i++;
 	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\'' || str[i] == '\"')
 		i++;
 	while (ft_isalnum(str[i]))
 	{
-		printf("str[i] == %c\n", str[i]);
+		// printf("str[i] == %c\n", str[i]);
 		tmp[j++] = str[i++];
 	}
+	printf("tmp == %s\n", tmp);
 	return(tmp);
 }
 
-char	*redir_out(char *str, int *i,char **env)
+char	*redir_in(char *str, int *i, int flag)
 {
 	int fd;
 	char *file_name;
 
-	file_name = str2str(str);
+	file_name = str2str(str, i, '<');
+
 	fd = open(file_name, O_RDONLY, 0644);
 	dup2(fd, 0);
-	return(file_name);
+	return(str);
+}
+
+char	*redir_out(char *str, int *i, int flag)
+{
+	int fd;
+	char *file_name;
+
+	file_name = str2str(str, i, '>');
+	if (flag)
+		fd = open(file_name , O_CREAT | O_APPEND | O_WRONLY, 0644);
+	else
+		fd = open(file_name , O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	if (fd)
+	dup2(fd, 1);
+	close(fd);
+	return (str);
 }
 
 void	parcer(char *str, char **env)
 {
 	int i = -1;
 	while (str[++i])
-	{
-		if (str[i] == '<')
-			str = redir_out();
-		if (str[i] == '<')
-			str = redir_in(str, &i, env);
+	{ 
+		if (str[i] == '<'){
+			if (str[i + 1] == '<'  && str[i + 2] != '<')
+			{
+				str = redir_in(str, &i, 1);
+			}
+			else if (str[i + 1] != '<')
+				str = redir_in(str, &i, 0);
+		}
+		if (str[i] == '>')
+			if (str[i + 1] == '>'  && str[i + 2] != '>')
+			{
+				str = redir_out(str, &i, 1);
+			}
+			else if (str[i + 1] != '<')
+				str = redir_out(str, &i, 0);
 	}
+	printf("str == %s\n", str);
 }
 
 int main(int argc, char *argv[], char **env)
 {
-	char *str = ft_strdup("cat < 1");
+	char *str = ft_strdup("cat < 1 <2");
 	parcer(str,env);
+
 	return 0;
 }
-
--- src/parser/test.c
+++ src/parser/test.c
@@ -6,7 +6,7 @@
 /*   By: rhoke <rhoke@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 13:46:30 by rhoke             #+#    #+#             */
-/*   Updated: 2022/01/04 19:18:32 by rhoke            ###   ########.fr       */
+/*   Updated: 2022/01/05 17:31:25 by rhoke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 int main(int argc, char const *argv[], char **envp)
 {
-	char *str = ft_strdup("cat -e test.c");
+	char *str = ft_strdup("cat -e");
+	int fd;
 	char **str1 = args_split(str, " ");
 
-
+	fd = open("test.c", O_RDONLY, 0644);
+	dup2(fd, 0);
+	close(fd);
 	// write(0, str, ft_strlen(str));
 	
 	execve(get_path(envp, str1[0]), str1, envp);
